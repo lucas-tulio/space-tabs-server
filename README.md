0501de7df9767b18636140989b072b24fc8b3292
Tulio <lucasdnd@gmail.com>
2016-03-22 12:46:58 -0300

    App settings, images endpoint

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # space-tabs-server
 Flask app that consumes the Astrobin.com API and serves its images to the Space Tabs extension
+
+### Setup
+
+1. `pip3 install -r requirements.txt`
+2. Include database and API info in `app.conf`
+3. `./run.py`
+
--- app/endpoints.py
+++ app/endpoints.py
@@ -3,5 +3,17 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return 'Hello, World!'
+
+@app.route('/images', methods=['GET'])
+def get_images():
+    return 'HUE'
+
+@app.errorhandler(404)
+def page_not_found(e):
+  return 'Error 404', 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+  return 'Error 500', 500
 
